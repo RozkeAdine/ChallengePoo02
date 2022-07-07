936de7ab6b8d9f6b8432649402850cb1f5c78a12
RozkeAdine <rozke-adine.bouyahia-std@donkey.school>
2022-07-07 16:26:01 +0200

    getting challenge poo 02

--- class/Truck.php
+++ class/Truck.php
@@ -3,7 +3,20 @@
 require_once 'class/Vehicle.php';
 
 class Truck extends Vehicle
-(
-    
-)
+{
+    private string $energy;
+    private int $loadCapacity;
+    private int $load;
+
+    public function fill(): string
+    {
+        $sentence = "";
+        while ($this->energyLevel > 0) {
+            $this->energyLevel--;
+            $sentence .= "in filling !!!";
+        }
+        $sentence .= "full !";
+        return $sentence;
+
+}
 ?>
