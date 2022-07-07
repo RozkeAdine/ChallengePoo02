ceb5bf6b0239ce91bf56c00c4d43353bbb632dcb
RozkeAdine <rozke-adine.bouyahia-std@donkey.school>
2022-07-07 18:02:35 +0200

    challenge poo02 finished

--- class/Truck.php
+++ class/Truck.php
@@ -5,18 +5,37 @@ require_once 'class/Vehicle.php';
 class Truck extends Vehicle
 {
     private string $energy;
-    private int $loadCapacity;
-    private int $load;
+    protected int $loadCapacity = 10;
+
+    public const ALLOWED_ENERGIES = [
+        'fuel',
+        'electric',
+    ];
+
+    public function starter(): string
+   {
+        $this->loadCapacity = 0;
+        return "Go !";
+    }
 
     public function fill(): string
     {
         $sentence = "";
-        while ($this->energyLevel > 0) {
-            $this->energyLevel--;
+        while ($this->loadCapacity < 10) {
+            $this->loadCapacity++;
             $sentence .= "in filling !!!";
         }
+
         $sentence .= "full !";
         return $sentence;
-
-}
+    }
+    public function getLoadCapacity(): int
+    {
+        return $this->loadCapacity;
+    }
+    public function setLoadCapacity( int $loadCapacity)
+    {
+        return $this->loadCapacity = $loadCapacity;
+    }
+};
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'class/Bicycle.php';
 require_once 'class/car.php';
+require_once 'class/truck.php';
 
 $bicycle = new Bicycle('blue', 1);
 echo $bicycle->forward();
@@ -11,4 +12,14 @@ $car = new Car('green', 4, 'electric');
 echo $car->forward();
 var_dump($car);
 var_dump(Car::ALLOWED_ENERGIES);
+
+
+$truck = new Truck('green', 8, 'fuel');
+
+echo $truck->starter();
+echo '<br> Chargement du camion : ' . $truck->getLoadCapacity() . '<br>';
+echo $truck->fill();
+echo '<br> Chargement du camion : ' . $truck->getLoadCapacity() . '<br>';
+echo $truck->fill();
+var_dump(Truck::ALLOWED_ENERGIES);
 ?>
